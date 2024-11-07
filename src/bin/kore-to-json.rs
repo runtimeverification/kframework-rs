1ef0a13cdf70c6a55bf1b1f97ef57c2a7c6429e7
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-11-07 16:55:17 +0100

    Replace `kore-parse` by `kore-to-json`

--- src/bin/kore-to-json.rs
+++ src/bin/kore-to-json.rs
@@ -7,7 +7,7 @@ use clap::Parser;
 use kframework::kore;
 
 #[derive(Parser)]
-/// Parse a .kore file
+/// Parse a textual KORE pattern from a file and convert it to JSON
 struct Args {
     /// Path of .kore file to parse
     path: PathBuf,
@@ -17,6 +17,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     let text = read_to_string(args.path)?;
     let mut parser = kore::Parser::new(&text)?;
-    let _definition = parser.definition()?;
+    let pattern = parser.pattern()?;
+    let json = serde_json::to_string(&pattern).map_err(|e| e.to_string())?;
+    print!("{}", json);
     Ok(())
 }
