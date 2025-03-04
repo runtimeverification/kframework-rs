0a3362fd3e0b65dc795cbc1a85cf46494ae5bb61
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2025-03-04 17:35:44 -0600

    Initial FFI interface

---
+++ kframework_ffi/src/kllvm.rs
@@ -0,0 +1,21 @@
+use std::ffi::{c_char, c_void};
+
+#[repr(C)]
+pub struct block;
+
+#[repr(C)]
+pub struct kore_pattern;
+
+#[allow(dead_code)]
+unsafe extern "C" {
+    pub fn kllvm_init() -> c_void;
+    pub fn kllvm_free_all_memory() -> c_void;
+
+    pub fn take_steps(steps: i64, subject: *const block) -> *const block;
+
+    pub fn kore_pattern_parse(data: *const c_char) -> *const kore_pattern;
+    pub fn kore_pattern_dump(pattern: *const kore_pattern) -> *const c_char;
+
+    pub fn kore_pattern_construct(pattern: *const kore_pattern) -> *const block;
+    pub fn kore_pattern_from_block(subject: *const block) -> *const kore_pattern;
+}
