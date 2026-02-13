88c3d131d5593de63152d3ccbb00cd6fddd2252c
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2026-02-13 13:57:27 -0600

    Re-arrange cargo package into workspace

--- kframework_core/src/bin/kore-to-json.rs
+++ kframework_core/src/bin/kore-to-json.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
-use kframework::kore;
+use kframework_core::kore;
 
 #[derive(Parser)]
 /// Parse a textual KORE pattern from a file and convert it to JSON
