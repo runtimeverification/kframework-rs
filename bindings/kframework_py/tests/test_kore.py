595506bc89a6a8c0662622af184ad871cb179a64
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2026-02-26 19:03:21 -0600

    Tests

--- bindings/kframework_py/tests/test_kore.py
+++ bindings/kframework_py/tests/test_kore.py
@@ -1,4 +1,4 @@
-from kframework_py.kore.syntax import Id
+from kframework_py.kore.syntax import Id, Sort, SortVar, SortApp
 
 def test_id():
     i = Id("foo")
@@ -10,3 +10,22 @@ def test_id():
 
     # TODO: Fix this!
     # assert a is b
+
+def test_sort():
+    # Parsing
+    sort = Sort.parse("SortA{SortB{SortC}, SortD, SortE{SortF, SortG{}}}")
+
+    # Getters and Constructors don't re-allocate
+    arg0 = sort.args[0]
+    arg1 = sort.args[1]
+    reused = SortApp("Reused", (arg0, arg1))
+    assert arg0 is sort.args[0]
+    assert arg1 is reused.args[1]
+
+    # Dataclass repr
+    expected_repr = "SortApp(name='SortB', args=(SortVar(name='SortC'),))"
+    assert arg0.__repr__() == expected_repr
+
+    # Structural equivalence
+    app = SortApp("SortB", (SortVar("SortC"),))
+    assert arg0 == app
