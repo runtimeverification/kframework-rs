18cd36b61c4efd24b8d01e1c0a45a35a9fc8a64d
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2026-02-27 23:00:25 -0600

    Update test

--- bindings/kframework_py/tests/test_kore.py
+++ bindings/kframework_py/tests/test_kore.py
@@ -5,14 +5,14 @@ def test_sort():
     sort = Sort.parse("SortA{SortB{SortC}, SortD, SortE{SortF, SortG{}}}")
 
     # Getters and Constructors don't re-allocate
-    arg0 = sort.args[0]
-    arg1 = sort.args[1]
+    arg0 = sort.sorts[0]
+    arg1 = sort.sorts[1]
     reused = SortApp("Reused", (arg0, arg1))
-    assert arg0 is sort.args[0]
-    assert arg1 is reused.args[1]
+    assert arg0 is sort.sorts[0]
+    assert arg1 is reused.sorts[1]
 
     # Dataclass repr
-    expected_repr = "SortApp(name='SortB', args=(SortVar(name='SortC'),))"
+    expected_repr = "SortApp(name='SortB', sorts=(SortVar(name='SortC'),))"
     assert arg0.__repr__() == expected_repr
 
     # Structural equivalence
